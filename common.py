7f4d1e07dd0e852d3251365b6025165350416660
jahangmar <48703856+jahangmar@users.noreply.github.com>
2020-10-18 11:49:06 +0200

    learning rate schedule, cleaned, stacked lstm layers

--- common.py
+++ common.py
@@ -4,18 +4,17 @@ import argparse;
 import numpy as np;
 import tensorflow as tf;
 
-seq_length = 300#200#100
+seq_length = 100#100
 batch_size = 64#64
 buffer_size = 10000
 
 embedding_dim = 256#256
-rnn_units = 1024#8192#1024
+rnn_units = 1024#1024
 
 epochs=10
 
 parser = argparse.ArgumentParser()
 parser.add_argument('directory', nargs=1)
-
 
 def parse_args():
     args = parser.parse_args()
@@ -26,8 +25,6 @@ def parse_args():
 
     input_dir = os.path.join(directory, 'inputs')
     filenames = os.listdir(input_dir)
-
-    #filename = os.path.join(directory, 'input.txt')
 
     global vocab, char2idx, idx2char, itexts
     global texts
@@ -49,15 +46,16 @@ def parse_args():
     for text in texts:
         itexts.append(np.array([char2idx[c] for c in text]))
 
-    #itext = np.array([char2idx[c] for c in text])
     return args
 
 
 def build_model(vocab_size, embedding_dim, rnn_units, batch_size):
         model = tf.keras.Sequential([
             tf.keras.layers.Embedding(vocab_size, embedding_dim, batch_input_shape=[batch_size, None]),
-            tf.keras.layers.GRU(rnn_units, return_sequences=True, stateful=True, recurrent_initializer='glorot_uniform'),
+#            tf.keras.layers.GRU(rnn_units, return_sequences=True, stateful=True, recurrent_initializer='glorot_uniform'),
             tf.keras.layers.LSTM(rnn_units, return_sequences=True, stateful=True, recurrent_initializer='glorot_uniform'),
+            tf.keras.layers.LSTM(rnn_units, return_sequences=True, stateful=True, recurrent_initializer='glorot_uniform'),
+#            tf.keras.layers.GRU(rnn_units, return_sequences=True, stateful=True, recurrent_initializer='glorot_uniform'),
 #            tf.keras.layers.SimpleRNN(rnn_units, return_sequences=True, stateful=True, recurrent_initializer='glorot_uniform'),
             tf.keras.layers.Dense(vocab_size)
         ])            
--- train_model.py
+++ train_model.py
@@ -6,19 +6,16 @@ import time
 from common import *;
 
 parser.add_argument('--epochs', nargs=1, default=epochs, type=int)
+parser.add_argument('--seq_length', nargs=1, type=int, default=seq_length)
 #parser.add_argument('--load_model', nargs=1, default=-1, type=int)
 args = parse_args()
 epochs = args.epochs[0]
-#load_model_num = args.load_model[0]
+seq_length = args.seq_length[0]
 from common import checkpoint_dir, texts, vocab, char2idx, idx2char, itexts;
-
-#examples_per_epoch = len(text)
 
 char_datasets = []
 for itext in itexts:
     char_datasets.append(tf.data.Dataset.from_tensor_slices(itext))
-
-#char_dataset = tf.data.Dataset.from_tensor_slices(itext)
 
 #for each chunk, form input and target sequence
 def split_input_target(chunk):
@@ -34,8 +31,6 @@ def create_single_dataset(c_dataset):
 dataset = create_single_dataset(char_datasets[0])
 for char_dataset in char_datasets[1:]:
     dataset = dataset.concatenate(create_single_dataset(char_dataset))
-
-#dataset = sequences.map(split_input_target)
 
 dataset = dataset.shuffle(buffer_size).batch(batch_size, drop_remainder=True)
 
@@ -54,7 +49,13 @@ else:
 def loss(labels, logits):
     return tf.keras.losses.sparse_categorical_crossentropy(labels, logits, from_logits=True)
 
-model.compile(optimizer='adam', loss=loss)
+lr_schedule = tf.keras.optimizers.schedules.PolynomialDecay(
+        initial_learning_rate=0.001,
+        end_learning_rate=0.00001,
+        decay_steps=10000
+        )
+
+model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=lr_schedule, amsgrad=False), loss=loss)
 
 
 checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
